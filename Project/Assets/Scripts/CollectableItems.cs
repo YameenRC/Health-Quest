fe0a4b30cbb77555137aec6adcf511b7e751033b
YameenRC <y.r.choudhury@se20.qmul.ac.uk>
2024-02-05 18:26:36 +0000

    Collectable Items and text

---
+++ Project/Assets/Scripts/CollectableItems.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableItems : MonoBehaviour
+{
+    private int fruit = 3;
+    [SerializeField] private Text Health;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectable")) 
+        {
+            Destroy(collision.gameObject);
+            fruit++;
+            Health.text = "Health: " + fruit;
+        }
+    }
+}
